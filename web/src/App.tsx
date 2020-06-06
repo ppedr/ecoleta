1b3f9c337799c89cd001a8120f46b2b9b2cff10a
ppedr <ppedrofreitas@outlook.com.br>
2020-06-06 18:30:04 -0300

    frontend web v.1

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Routes from './routes';
+
+// JSX: SIntaxe de XML dentro do js
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
